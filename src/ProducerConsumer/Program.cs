ae640e523210549592b65c81c05e062cb798fe26
dominikus1993 <dominikus1910@gmail.com>
2021-07-31 19:38:02 +0200

    small changes

--- src/ProducerConsumer/Program.cs
+++ src/ProducerConsumer/Program.cs
@@ -8,6 +8,8 @@ using System.Threading.Tasks;
 using Open.ChannelExtensions;
 namespace ProducerConsumer
 {
+
+    record Data(Guid Id, DateTime Date);
     class Program
     {
         private static ChannelReader<Guid> Producer(CancellationToken cancellationToken = default)
@@ -15,12 +17,38 @@ namespace ProducerConsumer
             var chan = Channel.CreateBounded<Guid>(new BoundedChannelOptions(1) { SingleWriter = false, SingleReader = false });
             async Task Produce(ChannelWriter<Guid> writer, CancellationToken token)
             {
-               var lines = await File.ReadAllLinesAsync("./data.txt");
-               foreach (var line in lines)
-               {
-                    if(Guid.TryParse(line, out Guid guid))
+                var lines = await File.ReadAllLinesAsync("./data.txt");
+                foreach (var line in lines)
+                {
+                    if (Guid.TryParse(line, out Guid guid))
                         await writer.WriteAsync(guid);
-               }
+                }
+            }
+
+            Task.Run(async () =>
+            {
+                try
+                {
+                    await Produce(chan, cancellationToken);
+                    chan.Writer.Complete();
+                }
+                catch (Exception ex)
+                {
+                    chan.Writer.Complete(ex);
+                }
+
+            }, cancellationToken);
+            return chan.Reader;
+        }
+
+        static ChannelReader<Data> PrepareData(ChannelReader<Guid> guids, CancellationToken cancellationToken = default)
+        {
+            var chan = Channel.CreateBounded<Data>(new BoundedChannelOptions(1) { SingleWriter = false, SingleReader = false });
+            async Task Produce(ChannelWriter<Data> writer, CancellationToken token)
+            {
+                await foreach(var id in guids.ReadAllAsync()) {
+                    
+                }
             }
 
             Task.Run(async () =>
